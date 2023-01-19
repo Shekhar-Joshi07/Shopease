816d7a9616768a4dbd5f9e91125e526579e43056
Vikram Navik <vikramnavik98@gmail.com>
2023-01-19 23:56:06 +0530

    day3first

--- shopease/src/Components/SingleMobileCard.jsx
+++ shopease/src/Components/SingleMobileCard.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Button, Flex, ListItem, Text, UnorderedList } from '@chakra-ui/react';
 import axios from 'axios';
 import React from 'react'
 import { useEffect } from 'react';
@@ -20,11 +20,27 @@ const SingleMobileCard = () => {
     },[id]);
 
   return (
-    <Flex>
-        <Box>
-            <img src={mobdata.img_section} alt={mobdata.id} />
+    <Flex w="200rem">
+        <Box w="10%">
+            <img width="100%" src={mobdata.img_section} alt={mobdata.id} />
         </Box>
-        <Box></Box>
+        <Box lineHeight="3rem">
+            <Text fontSize='3xl'>{mobdata.title}</Text>
+            <Text fontSize='xl' color="#4ca8b6" textAlign="left">{mobdata.brand}</Text>
+            <Flex gap={2} justifyContent="left" alignItems="center">
+              <Text as="b" fontSize="2xl">{mobdata.p_price}</Text>
+              <Text fontSize='md' as="del">{mobdata.old_prices}</Text>
+              <Text fontSize="xl" color="#4ca8b6">{mobdata.prd_discount}</Text>
+            </Flex>
+            <UnorderedList color="gray.400">
+              <ListItem>Display Size : 1.8 inches (4.57 cm)</ListItem>
+              <ListItem>Warranty Available : Manufacturer Warranty</ListItem>
+            </UnorderedList>
+            <Flex gap={5}>
+            <Button px="3rem" py="1.5rem" color="#ff7856" colorScheme='#ff7856' variant='outline'>ADD TO CART</Button>
+            <Button px="3rem" py="1.5rem" color="white"  bgColor="#ff7856" variant='solid'>BUY NOW</Button>
+            </Flex>
+        </Box>
     </Flex>
   )
 }
