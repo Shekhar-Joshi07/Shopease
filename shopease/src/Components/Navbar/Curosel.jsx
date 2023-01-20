8b22c8726683ca3f5ff7c7ef9450ceb747f003ae
harshavardhan1612 <panchireddyharsha1612@gmail.com>
2023-01-20 11:01:44 +0530

    navbar

---
+++ shopease/src/Components/Navbar/Curosel.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Curosel = () => {
+  return (
+    <div>Curosel</div>
+  )
+}
--- shopease/src/index.js
+++ shopease/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { ChakraProvider } from '@chakra-ui/react'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ChakraProvider>
     <App />
-  </React.StrictMode>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
