52064f577e8441d7d7d0eda127e209017bb978d6
Vikram Navik <vikramnavik98@gmail.com>
2023-01-22 16:24:02 +0530

    success changes

--- shopease/src/Pages/Success.jsx
+++ shopease/src/Pages/Success.jsx
@@ -1,8 +1,14 @@
+import { Alert, AlertIcon, Box, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const Success = () => {
   return (
-    <div>Success</div>
+    <Box>
+         <Alert status='success' height="30rem" m="3rem">
+            <AlertIcon />
+            <Text as="b" fontSize="4xl">Congrats! Payment Successful.</Text>
+          </Alert>
+    </Box>
   )
 }
 
